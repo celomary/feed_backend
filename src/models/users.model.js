e4be4f46c5ab0e1167b831d532158208ad44c706
Kyle Padayachee <kyle44987@gmail.com>
2023-01-23 15:22:02 +0200

    created service model into service branch

---
+++ src/models/users.model.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+  },
+  googleId: String,
+  skills: [String],
+  location: String,
+  imageProfile: String,
+  acceptNewClient: {
+    isAccepting: Boolean,
+    endDate: Date,
+  },
+});
+
+module.exports = model("User", UserSchema);
